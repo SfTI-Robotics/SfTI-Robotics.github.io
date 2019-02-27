bd616978e6c37f1f7f706baf50e702afc583bce0
Yonghui Tao <ytao624@aucklanduni.ac.nz>
2019-02-27 15:24:47 +1300

    update cartpole dqn

--- ScenarioComparsion/CartPole/DQN/DQN-cartPole-greg.py
+++ ScenarioComparsion/CartPole/DQN/DQN-cartPole-greg.py
@@ -112,21 +112,20 @@ class DQNSolver:
         #     self.model.fit(state, target_f,batch_size=BATCH_SIZE,epochs=1, verbose = 0)
 
          # ======================================================
-        update_input = np.zeros((BATCH_SIZE, self.observation_space))
-        update_target = np.zeros((BATCH_SIZE, self.observation_space))
+        states = np.zeros((BATCH_SIZE, self.observation_space))
+        next_states = np.zeros((BATCH_SIZE, self.observation_space))
         action, reward, done = [], [], []
 
         for i in range(BATCH_SIZE):
-            update_input[i] = batch[i][0]
+            states[i] = batch[i][0]
             action.append(batch[i][1])
             reward.append(batch[i][2])
-            update_target[i] = batch[i][3]
+            next_states[i] = batch[i][3]
             done.append(batch[i][4])
 
-        target = self.model.predict(update_input)
+        target = self.model.predict(states)
 
-        target_next = self.model.predict(update_target)
-
+        target_next = self.model.predict(next_states)
         for i in range(BATCH_SIZE):
             if done[i]:
                 target[i][action[i]] = reward[i]
@@ -134,7 +133,7 @@ class DQNSolver:
                 # bellman equation
                 target[i][action[i]] = reward[i] + GAMMA * np.amax(target_next[i])
 
-        self.model.fit(update_input, target, batch_size=BATCH_SIZE, epochs=1, verbose=0)
+        self.model.fit(states, target, batch_size=BATCH_SIZE, epochs=1, verbose=0)
 
          # ======================================================
         # state_array = np.zeros((BATCH_SIZE, self.observation_space)) 
@@ -160,11 +159,11 @@ class DQNSolver:
         # self.model.fit(state_array,out,batch_size = BATCH_SIZE, epochs=1,verbose=1)
 
          # ======================================================
-        print("finish replay")
+        # print("finish replay")
         # update exploration rate
         self.exploration_rate *= EXPLORATION_DECAY
         self.exploration_rate = max(EXPLORATION_MIN, self.exploration_rate)
-        print('e =', 1 - self.exploration_rate)
+        # print('e =', 1 - self.exploration_rate)
 
 
 
@@ -178,18 +177,24 @@ def cartpole():
     action_space = env.action_space.n
     dqn_solver = DQNSolver(observation_space, action_space)
     run = 0
+    episode_rewards = []
+    
     # for episode_counter in tqdm(range(200)):
-    for ep in tqdm(range(EPISODE)):
+    # for ep in tqdm(range(EPISODE)):
+    for ep in range(EPISODE):
         run += 1
         state = env.reset()
         state = np.reshape(state, [1, observation_space])
         step = 0
         start_time = time.time()
         reward_sum = 0
+        if ep % 20 == 0:
+            episode_rewards = []
 
         while True:
             step += 1
-            env.render()
+            if ep > 150:    
+                env.render()
             action = dqn_solver.act(state)
             state_next, reward, terminal, info = env.step(action)
             # Question: why reward = 0 at end of episode
@@ -201,9 +206,11 @@ def cartpole():
             state = state_next
 
             reward_sum += reward
-
+            episode_rewards.append(reward_sum)
+            
             if terminal:
-                # print "Run: " + str(run) + ", exploration: " + str(dqn_solver.exploration_rate) + ", score: " + str(step)
+                avg_score = np.mean(episode_rewards)
+                print("Episode: " + str(run) + ", epsilon: " + str(1 - dqn_solver.exploration_rate) + ", steps: " + str(step), ",  average score =", avg_score)
                 # score_logger.add_score(step, run)
                 break
             dqn_solver.experience_replay()
